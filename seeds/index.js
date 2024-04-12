663350434a811685cef6183f2146e5a2047f88be
Rishita Maheshwari <maheshwaririshita7@gmail.com>
2024-04-13 02:37:19 +0530

    design completed

--- seeds/index.js
+++ seeds/index.js
@@ -18,9 +18,13 @@ const seedDB =async()=>{
     await Campground.deleteMany({});
     for(let i=0;i<50;i++){
         const random1000=Math.floor(Math.random()*1000);
+        const price=Math.floor(Math.random()*1000)+100;
         const camp=new Campground({
             location:`${cities[random1000].city}, ${cities[random1000].state}`,
-            title:`${sample(descriptors)} ${sample(places)}`
+            title:`${sample(descriptors)} ${sample(places)}`,
+            image:'https://source.unsplash.com/collection/483251',
+            description:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium eaque veniam, voluptates a quam quas esse dolor non veritatis! Quaerat mollitia inventore consequatur recusandae ipsa repellendus praesentium, fugiat voluptatum pariatur.',
+            price
         })
         await camp.save();
     }
